2d95ca4f835d05989d3907f9965a2c3d9278ad06
Filip Mularski <filipelblag@gmail.com>
2019-10-27 13:20:04 +0100

    final touch

--- app.py
+++ app.py
@@ -5,14 +5,20 @@ import os
 root = tk.Tk()
 apps = []
 
+if os.path.isfile('save.txt'):
+    with open('save.txt', 'r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]
+
 
 def add_app():
     for widget in frame.winfo_children():   # removing all labels before adding all one again
         widget.destroy()
 
     filename = filedialog.askopenfile(initialdir='/', title='Select file', # file dialog retuns filename
-                                      filetypes=(('executables', '*.exe'), ('all files', '*.*'))).name
-    apps.append(filename)
+                                      filetypes=(('executables', '*.exe'), ('all files', '*.*')))
+    apps.append(filename.name)
 
     for app in apps:    # labeling all added apps
         label = tk.Label(frame, text=app, bg='gray')
@@ -36,5 +42,13 @@ openFileBtn.pack()
 runAppsBtn = tk.Button(root, text='Run apps', padx=10, pady=5, fg='white', bg='#263D42', command=run_apps)
 runAppsBtn.pack()
 
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
 tk.mainloop()
+
+with open('save.txt', 'w') as f:   # writing selected apps to text file after closing py app
+    for app in apps:
+        f.write(app + ',')
 
